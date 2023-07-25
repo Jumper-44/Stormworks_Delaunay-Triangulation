0385ee20db3817add6f1b2db5e76298ffeb4561b
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2023-07-25 02:13:53 +0200

    just some code that haven't been testet

--- 3d surface triangulation/3d triangulation.lua
+++ 3d surface triangulation/3d triangulation.lua
@@ -5,6 +5,7 @@ require("3d surface triangulation.kdtree")
 ---comment
 ---@return table
 SurfaceTriangulation = function()
+    ---@section GetCircumCircle
     ---Returns the circumcircle of three points that make up a triangle in 3d, so points are assumed to be non-collinear.
     ---Math from https://en.wikipedia.org/wiki/Circumcircle#Higher_dimensions
     ---@param pointA table
@@ -43,35 +44,51 @@ SurfaceTriangulation = function()
             (cpv_x * ca_cb_cross_y - cpv_y * ca_cb_cross_x) * inv_d
 
         return {
-            x = pointC[1] + dx,
-            y = pointC[2] + dy,
-            z = pointC[3] + dz,
-            r2 = dx * dx + dy * dy + dz * dz
+            pointC[1] + dx,                 -- x
+            pointC[2] + dy,                 -- y
+            pointC[3] + dz,                 -- z
+            dx * dx + dy * dy + dz * dz     -- r*r
         }
     end
+    ---@endsection
 
 
-    ---     C
-    ---    / \
-    ---   b   a
-    ---  /     \
-    --- A___c___B
+
+    local sub = function(p1, p2) return {p1[1] - p2[1], p1[2] - p2[2], p1[3] - p2[3]} end
+    local dot = function(p1, p2) return p1[1] * p2[1] + p1[2] * p2[2] + p1[3] * p2[3] end
+    local cross = function(p1, p2) return {p1[2] * p2[3] - p1[3] * p2[2], p1[3] * p2[1] - p1[1] * p2[3], p1[1] * p2[2] - p1[2] * p2[1]} end
+
+    ---Returns the circumsphere of four points that make up a tetrahedron, so points are assumed to be non-collinear.
+    ---Math from https://math.stackexchange.com/a/2414870
     ---@param pointA table
     ---@param pointB table
     ---@param pointC table
-    ---@param neighborTriangle_a table|false
-    ---@param neighborTriangle_b table|false
-    ---@param neighborTriangle_c table|false
+    ---@param pointD table
     ---@return table
-    local NewTriangle = function(pointA, pointB, pointC, neighborTriangle_a, neighborTriangle_b, neighborTriangle_c)
+    local GetCircumSphere = function(pointA, pointB, pointC, pointD)
+        local AB, AC, AD, AO = sub(pointB, pointA), sub(pointC, pointA), sub(pointD, pointA), {}
+
+        local AB_len2, AC_len2, AD_len2,  cross_AC_AD, cross_AD_AB, cross_AB_AC =
+            dot(AB, AB), dot(AC, AC), dot(AD, AD),
+            cross(AC, AD), cross(AD, AB), cross(AB, AC)
+
+        local inv_denominator = 0.5 / dot(AB, cross_AC_AD)
+
+        for i = 1, 3 do
+            AO[i] = (AB_len2 * cross_AC_AD[i] + AC_len2 * cross_AD_AB[i] + AD_len2 * cross_AB_AC[i]) * inv_denominator
+        end
+
         return {
-            pointA, pointB, pointC,
-            neighborTriangle_a, neighborTriangle_b, neighborTriangle_c,
-            GetCircumCircle(pointA, pointB, pointC), false
+            pointA[1] + AO[1],  -- x
+            pointA[2] + AO[2],  -- y
+            pointA[3] + AO[3],  -- z
+            dot(AO, AO)         -- r*r
         }
     end
 
+
     local kdtree = IKDTree(3)
+    local vertices = {}
     local triangle_action_queue = { -- https://www.lua.org/pil/11.4.html
         first = 0; last = -1;
 
@@ -89,60 +106,112 @@ SurfaceTriangulation = function()
             return value
         end
     }
-    local point_minimum_neighboring_len2 = 0.25^2
-    local point_maximum_neighboring_len2 = 10^2
+
+    local NewTriangle = function(pointA, pointB, pointC)
+        return {pointA, pointB, pointC, isSurface = false}
+    end
+
+    local NewTetrahedron = function(pointA, pointB, pointC, pointD)
+        local sphere = GetCircumSphere(pointA, pointB, pointC, pointD)
+
+        local tetrahedron = {
+            vertices = {pointA, pointB, pointC, pointD},
+            triangles = {false, false, false, false},
+            neighbors = {false, false, false, false},
+            circumSphere = sphere,
+            isInvalid = false,
+            isChecked = false
+        }
+
+        sphere.tetrahedron = tetrahedron
+        kdtree.IKDTree_insert(sphere)
+
+        return tetrahedron
+    end
+
+    do -- Init super-tetrahedron
+        local p1, p2, p3, p4 =
+            {0,     5e5,    0,      id = 0},
+            {-9e5,  -3e4,   -9e5,   id = 0},
+            {9e5,   -3e4,   9e5,    id = 0},
+            {0,     -3e4,   9e5,    id = 0}
+        local superTerahedron = NewTetrahedron(p1, p2, p3, p4)
+        superTerahedron.triangles = {
+            NewTriangle(p2, p3, p4),
+            NewTriangle(p3, p4, p1),
+            NewTriangle(p4, p1, p2),
+            NewTriangle(p1, p2, p3)
+        }
+    end
 
 
     return {
+        vertices = vertices;
         triangle_action_queue = triangle_action_queue;
 
-        ---Returns true if point got inserted else false
-        ---@param point table
-        ---@return boolean
         insert = function(point)
-            local neighbors = kdtree.IKDTree_nearestNeighbors(point, 5)
+            -- Do Bowyer-Watson Algorithm
+            local vertices_size, nearestCircumCenter_tetrahedron = #vertices + 1, kdtree.IKDTree_nearestNeighbors(point, 1).tetrahedron
+            vertices[vertices_size] = point
+            point.id = vertices_size
 
-            -- If no neighboring point exist then add point to k-d tree
-            -- If neighboring point exist then test len2 to nearest point and add if greater than point_minimum_neighboring_len2.
-            if not neighbors[1] or neighbors[1].len2 > point_minimum_neighboring_len2 then
-                point.triangles = {}
-                kdtree.IKDTree_insert(point)
+            nearestCircumCenter_tetrahedron.isChecked = true
+            local invalid_tetrahedra, tetrahedra_check_queue, tetrahedra_check_queue_pointer = {}, {nearestCircumCenter_tetrahedron}, 1
 
-                if #neighbors >= 2 then -- and #neighbors[1].triangles == 0 and #neighbors[2].triangles == 0 then
-                    local new_triangle = NewTriangle(point, neighbors[1], neighbors[2], false, false, false)
-                    point.triangles[1] = new_triangle
-                    neighbors[1].triangles[1] = new_triangle
-                    neighbors[2].triangles[1] = new_triangle
-                    triangle_action_queue:pushleft(new_triangle, true)
+            -- Find all invalid tetrahedra
+            repeat
+                local current_tetrahedron = tetrahedra_check_queue[tetrahedra_check_queue_pointer]
+
+                -- Is current_tetrahedron invalid?
+                if kdtree.len2(point, current_tetrahedron.circumSphere) < current_tetrahedron.circumSphere[4] then
+                    current_tetrahedron.isInvalid = true
+                    invalid_tetrahedra[#invalid_tetrahedra+1] = current_tetrahedron
+
+                    current_tetrahedron.circumSphere.tetrahedron = nil
+                    kdtree.IKDTree_remove(current_tetrahedron.circumSphere)
                 end
 
-                
-                --[[
-                -- Check if new point makes any near triangles illegal and then try boywer-watson algorithm
-                local illegal_triangles_queue = {}
-                local checked_triangles = {}
+                -- If current_tetrahedron is invalid OR no invalid tetrahedra has been found yet then try add neighboring tetrahedra to queue
+                if current_tetrahedron.isInvalid or #invalid_tetrahedra == 0 then
+                    for i = 1, 4 do
+                        local current_neighbor = current_tetrahedron.neighbors[i]
 
-                for i = 1, #neighbors do
-                    for j = 1, #neighbors[i].triangles do
-                        for k = 1, #checked_triangles do
-                            if neighbors[i].triangles[j] == checked_triangles[k] then goto continue end
+                        -- if neighbor exist and has not been checked yet then add to queue
+                        if current_neighbor and not current_neighbor.isChecked then
+                            tetrahedra_check_queue[#tetrahedra_check_queue+1] = current_neighbor
+                            current_neighbor.isChecked = true
                         end
-
-                        checked_triangles[#checked_triangles+1] = neighbors[i].triangles[j]
-
-                        if kdtree.len2(point, neighbors[i].triangles[j][7]) < neighbors[i].triangles[j][7].r2 then
-                            illegal_triangles_queue[#illegal_triangles_queue+1] = neighbors[i].triangles[j]
-                        end
-                        
-                        ::continue::
                     end
                 end
-                --]]
 
+                tetrahedra_check_queue_pointer = tetrahedra_check_queue_pointer + 1
+            until #tetrahedra_check_queue < tetrahedra_check_queue_pointer
 
-                return true
+            -- reset isChecked state for checked tetrahedra
+            for i = 1, #tetrahedra_check_queue do
+                tetrahedra_check_queue[i].isChecked = false
             end
-            return false
+
+            -- Now the invalid_tetrahedra makes up a polyhedron
+            -- Find all facets that are not shared in invalid_tetrahedra
+            local facets, facet_tetrahedron_neighbor = {}, {}
+
+            for i = 1, #invalid_tetrahedra do
+                local current_tetrahedron = invalid_tetrahedra[i]
+                for j = 1, 4 do
+                    local current_neighbor = current_tetrahedron.neighbors[j]
+
+                    -- If facet doesn't have neighbor OR if neighbor exist and it is not invalid then add facet to table
+                    if not current_neighbor or not current_neighbor.isInvalid then
+                        local facets_size = #facets + 1
+                        facets[facets_size] = current_tetrahedron.triangles[j]
+                        facet_tetrahedron_neighbor[facets_size] = current_neighbor
+                    end
+                end
+            end
+
+            -- Construct new tetrahedra
+            -- TODO
         end;
     }
 end
