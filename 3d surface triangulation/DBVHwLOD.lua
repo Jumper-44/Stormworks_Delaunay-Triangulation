7c87c011cb743afc2847fe3b48651e9845db4364
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2023-08-28 00:33:56 +0200

    init

---
+++ 3d surface triangulation/DBVHwLOD.lua
@@ -0,0 +1,23 @@
+-- GitHub: https://github.com/Jumper-44
+
+---Dynamic Bounding Volume Hierarchy with Level Of Detail.
+---Specifically for triangles part of a mesh.
+---@return table
+DBVHwLOD = function()
+
+
+    
+    return {
+        insert = function()
+
+        end;
+
+        remove = function()
+
+        end;
+
+        frustumCull = function(cameraTransform)
+
+        end
+    }
+end
